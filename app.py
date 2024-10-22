436d51a13eb2ea4fe70e23382b999261fd1e34a9
cechad <cesarescobar2812@gmail.com>
2024-10-21 23:36:57 -0600

    [MOD] se modifico puerto

--- app.py
+++ app.py
@@ -323,4 +323,5 @@ def logout():
 # Ejecuta la aplicación
 # ========================================
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
+
